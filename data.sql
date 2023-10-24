7179b8947d32e85699bb0928c4a9400cdac4d2d0
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-24 07:37:46 +0530

    Update schema.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name , date_of_birth, weight_kg, neutered, escape_attempts)
-vet_clinic-# VALUES ('Agumon','2020-02-03', 10.23, TRUE, 0);
+VALUES ('Agumon','2020-02-03', 10.23, TRUE, 0);
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Gabumon', '2018-11-15', 8, TRUE, 2);
