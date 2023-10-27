4f2a5179b44fe3339154a93da709c77d35e45359
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-27 16:15:11 +0530

    Update data.sql and schema.sql

--- data.sql
+++ data.sql
@@ -22,3 +22,32 @@ VALUES
 ('Boarmon', '2005-06-07', 20.40, TRUE, 7, 'Unspecified'),
 ('Blossom', '1998-10-13', 17.00, TRUE, 3, 'Unspecified'),
 ('Ditto', '2022-05-14', 22.00, TRUE, 4, 'Unspecified');
+
+--3
+INSERT INTO owners (full_name, age) VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+
+UPDATE animals
+SET
+    species_id = (CASE
+        WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+        ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+    END),
+    owner_id = (CASE
+        WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+        WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+        WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+        WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+        WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+        ELSE NULL
+    END);
--- schema.sql
+++ schema.sql
@@ -9,5 +9,23 @@ CREATE TABLE animals (
     weight_kg decimal(5,2)
 );
 
+--2
+
 ALTER TABLE animals
 ADD species VARCHAR(100);
+
+--3
+CREATE TABLE owners (
+    id serial PRIMARY KEY,
+    full_name varchar(255),
+    age integer
+);
+
+CREATE TABLE species (
+    id serial PRIMARY KEY,
+    name varchar(255)
+);
+
+ALTER TABLE animals
+ADD COLUMN species_id integer REFERENCES species(id),
+ADD COLUMN owner_id integer REFERENCES owners(id);
